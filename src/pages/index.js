64b800d81d937f73d35c19d605925ffdd7203c52
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-02 19:12:00 -0600

    linked titles to the actual pages of the blog and created their styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
-import * as React from "react"
-import { Link, graphql } from "gatsby"
+import React from "react"
+import { graphql, Link } from "gatsby"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
 
 const IndexPage = ({data}) => {
   console.log(data)
@@ -16,7 +26,9 @@ const IndexPage = ({data}) => {
         {
           data.allMarkdownRemark.edges.map(({node}) => (
             <div key={node.id}>
-              <span>{node.frontmatter.title} - {node.frontmatter.date}</span>
+              <BlogLink to={node.fields.slug}>
+              <BlogTitle>{node.frontmatter.title} - {node.frontmatter.date}</BlogTitle>
+              </BlogLink>
               <p>{node.excerpt}</p>
 
             </div>
@@ -30,20 +42,23 @@ const IndexPage = ({data}) => {
 export default IndexPage
 
 export const query = graphql`
-query {
-  allMarkdownRemark {
-    totalCount
-    edges {
-      node {
-        id
-        frontmatter {
-          date
-          description
-          title
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            description
+            title
+          }
+          fields {
+            slug
+          }
+          excerpt
         }
-        excerpt
       }
     }
   }
-}
 `
