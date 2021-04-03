8c22f4276014c76f38009570ca9c669249a32bdb
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-02 19:02:51 -0600

    generated pages dynamically based on a template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,45 @@
+const path = require(`path`)
 const {createFilePath} = require(`gatsby-source-filesystem`)
 
-
 exports.onCreateNode = ({node, getNode, actions}) => {
-    const {createNodeField} = actions
-   if(node.internal.type === `MarkdownRemark`) {
-const slug = createFilePath({node, getNode })
+  const {createNodeField} = actions;
+  if(node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({node, getNode })
 
-createNodeField({
-    node,
-    name: `slug`,
-    value: slug
-})
-   }
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    })
+  }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  return graphql(`
+    {
+      query {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({node}) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/components/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug
+        }
+      })
+    })
+  })
 }
---
+++ src/components/templates/blog-post.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../layout'
+
+
+
+export default ({data}) => {
+  const post = data.markdownRemark;
+
+  return (
+      <Layout>
+          <div>
+              <h1>{post.frontmattter.title}</h1>
+              <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          </div>
+      </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+      markdownRemark( fields: { slug: { eq: $slug }}) {
+          html
+          frontmatter {
+            title
+          }
+      }
+  }
+`
